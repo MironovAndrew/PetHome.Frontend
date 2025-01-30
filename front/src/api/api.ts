79a51f469879e3aae08bb6a550ae37991b604b54
Андрей Смирный <smirnay2001@mail.ru>
2025-01-30 22:39:02 +0300

    Применил адрес nginx в API_URL

--- front/src/api/api.ts
+++ front/src/api/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios"; 
 
-export const API_URL: string = "http://localhost:5258/AccountAuthentication/";
+export const API_URL: string = "http://localhost/api/AccountAuthentication";
  export const api = axios.create({
     baseURL: API_URL, 
      withCredentials:true
